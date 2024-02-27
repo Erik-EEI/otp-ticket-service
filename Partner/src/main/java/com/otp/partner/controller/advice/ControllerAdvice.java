071858cf175a23cecfdf71536629bb0ab7de7f1c
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-27 22:31:43 +0100

    Create and integrate custom exception handling for scenario where user trying to reserve to already taken seat

--- Partner/src/main/java/com/otp/partner/controller/advice/ControllerAdvice.java
+++ Partner/src/main/java/com/otp/partner/controller/advice/ControllerAdvice.java
@@ -2,18 +2,25 @@ package com.otp.partner.controller.advice;
 
 import com.otp.partner.dto.response.ApiErrorResponse;
 import com.otp.partner.exception.EventNotFoundException;
+import com.otp.partner.exception.SeatAlreadyReservedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.otp.partner.controller")
-public class EventControllerAdvice {
+public class ControllerAdvice {
 
     @ExceptionHandler(EventNotFoundException.class)
     public ResponseEntity<ApiErrorResponse> handleEventNotFoundException(EventNotFoundException ex) {
         ApiErrorResponse response = new ApiErrorResponse(ex.getMessage(), ex.getErrorCode(),false);
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SeatAlreadyReservedException.class)
+    public ResponseEntity<ApiErrorResponse> handleSeatAlreadyReservedException(SeatAlreadyReservedException ex) {
+        ApiErrorResponse response = new ApiErrorResponse(ex.getMessage(), ex.getErrorCode(),false);
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(Exception.class)
