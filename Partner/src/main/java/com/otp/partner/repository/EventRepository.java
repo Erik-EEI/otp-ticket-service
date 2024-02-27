7f1660bba54a83c190b98b0c2fded25de5a8ac89
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-27 16:50:42 +0100

    Create event and seat repositories

---
+++ Partner/src/main/java/com/otp/partner/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.otp.partner.repository;
+
+import com.otp.partner.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
---
+++ Partner/src/main/java/com/otp/partner/repository/SeatRepository.java
@@ -0,0 +1,12 @@
+package com.otp.partner.repository;
+
+import com.otp.partner.entity.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+    List<Seat> findByEventId(Long eventId);
+}
