ce0f3ed9220540cdca84887c987f9545aed75dbb
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-28 11:32:20 +0100

    Add method to tokenservice that can retrieve the user connected to the provided token

--- TicketService/src/main/java/com/otp/ticketservice/core/service/TokenService.java
+++ TicketService/src/main/java/com/otp/ticketservice/core/service/TokenService.java
@@ -1,5 +1,6 @@
 package com.otp.ticketservice.core.service;
 
+import com.otp.ticketservice.core.entity.User;
 import com.otp.ticketservice.core.entity.UserToken;
 import com.otp.ticketservice.core.repository.UserTokenRepository;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,12 @@ public class TokenService {
                 .orElseThrow(RuntimeException::new); //TODO Replace with custom exception
 
         return true;
+    }
+
+    public User getUserFromToken( String token ){
+        UserToken userToken = userTokenRepository.findUserTokenByToken(token)
+                .orElseThrow(RuntimeException::new);
+
+        return userToken.getUser();
     }
 }
