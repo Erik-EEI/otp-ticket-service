b98fc648f66b0277ac6ad437c42cbcbf5068fbad
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-28 09:57:41 +0100

    Create user entity

---
+++ TicketService/src/main/java/com/otp/ticketservice/core/entity/User.java
@@ -0,0 +1,18 @@
+package com.otp.ticketservice.core.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
