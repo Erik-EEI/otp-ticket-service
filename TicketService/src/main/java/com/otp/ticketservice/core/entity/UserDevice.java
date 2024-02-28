c8a1bea3a06f02a4cd7621574251c7e2653dfc25
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-28 10:02:32 +0100

    Create userDevice db entity

---
+++ TicketService/src/main/java/com/otp/ticketservice/core/entity/UserDevice.java
@@ -0,0 +1,24 @@
+package com.otp.ticketservice.core.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_devices")
+public class UserDevice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    private String deviceHash;
+}
