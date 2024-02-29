23fbbb3e8a6ea215d1871ac17d82246247d64704
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-29 13:08:58 +0100

    Resolve time formatting and comparing errors

--- TicketService/src/main/java/com/otp/ticketservice/ticket/utils/TimestampHandler.java
+++ TicketService/src/main/java/com/otp/ticketservice/ticket/utils/TimestampHandler.java
@@ -12,11 +12,12 @@ import java.util.Date;
 @UtilityClass
 public class TimestampHandler {
     public static void checkStartTime(String timestampStr){
-        String dateString = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (Long.parseLong(timestampStr)*1000));
-        LocalDateTime eventDate = LocalDateTime.parse(dateString);
+        String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date (Long.parseLong(timestampStr)*1000));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        LocalDateTime eventDate = LocalDateTime.parse(dateString,formatter);
         LocalDateTime currentDate = LocalDateTime.now();
 
-        if(currentDate.isAfter(eventDate)) throw new RuntimeException(); //TODO Replace with custom exception
-
+        if(currentDate.isAfter(eventDate)) throw new RuntimeException(""); //TODO Replace with custom exception
     }
 }
