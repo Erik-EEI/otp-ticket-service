549801bd4340c0a3dafe9cf88d717c92ceb6421a
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-29 21:21:44 +0100

    Enable caching in ticket module

--- TicketService/src/main/java/com/otp/ticketservice/TicketServiceApplication.java
+++ TicketService/src/main/java/com/otp/ticketservice/TicketServiceApplication.java
@@ -3,9 +3,11 @@ package com.otp.ticketservice;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class TicketServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TicketServiceApplication.class, args);
--- TicketService/src/main/java/com/otp/ticketservice/ticket/service/EventService.java
+++ TicketService/src/main/java/com/otp/ticketservice/ticket/service/EventService.java
@@ -3,7 +3,6 @@ package com.otp.ticketservice.ticket.service;
 import com.otp.ticketservice.api.dto.EventDataDTO;
 import com.otp.ticketservice.ticket.dao.EventDAO;
 import com.otp.ticketservice.ticket.dto.detailed_event.DetailedEventDTO;
-import com.otp.ticketservice.ticket.dto.event_list.EventDTO;
 import com.otp.ticketservice.ticket.dto.event_list.EventListDTO;
 import com.otp.ticketservice.ticket.dto.single_event_with_seats.EventSeatsDTO;
 import com.otp.ticketservice.ticket.interfaces.EventServiceInterface;
@@ -13,7 +12,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.net.http.HttpResponse;
-import java.util.Objects;
 
 @Service
 public class EventService implements EventServiceInterface {
