b288f3ca546655d5ef7b50720775f2f374551a26
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-02-29 21:00:50 +0100

    Add caching to ticket module All eventsDAO

--- TicketService/src/main/java/com/otp/ticketservice/ticket/dao/EventDAO.java
+++ TicketService/src/main/java/com/otp/ticketservice/ticket/dao/EventDAO.java
@@ -3,6 +3,7 @@ package com.otp.ticketservice.ticket.dao;
 import com.otp.ticketservice.api.dto.EventDataDTO;
 import com.otp.ticketservice.ticket.utils.HttpRequestUtil;
 import com.otp.ticketservice.ticket.utils.UrlBuilder;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.net.http.HttpResponse;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @Repository
 public class EventDAO {
+    @Cacheable("events")
     public HttpResponse<String> getAllEvents(){
         String url = UrlBuilder.buildUrl("getEvents");
 
