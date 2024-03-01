0d43e639e889eff2927e9a15a79fd583b6196c2c
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-03-01 16:51:31 +0100

    Modify dockerfiles so it will build the app first, and then copies it

--- TicketService/src/main/java/com/otp/ticketservice/TicketServiceApplication.java
+++ TicketService/src/main/java/com/otp/ticketservice/TicketServiceApplication.java
@@ -1,10 +1,8 @@
 package com.otp.ticketservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
