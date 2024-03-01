8ebe918c273e944beaf40f4ca63ed083f1300852
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-03-01 11:47:29 +0100

    Implement logging of core service operations

--- TicketService/src/main/java/com/otp/ticketservice/core/service/CoreService.java
+++ TicketService/src/main/java/com/otp/ticketservice/core/service/CoreService.java
@@ -7,12 +7,15 @@ import com.otp.ticketservice.core.interfaces.CoreServiceInterface;
 import com.otp.ticketservice.core.interfaces.TokenServiceInterface;
 import com.otp.ticketservice.core.service.card.CardService;
 import com.otp.ticketservice.core.service.token.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CoreService implements CoreServiceInterface {
     private final CardServiceInterface cardService;
     private final TokenServiceInterface tokenService;
+    private final Logger LOGGER = LoggerFactory.getLogger("[CORE MODULE]");
 
     public CoreService(CardService cardService, TokenService tokenService) {
         this.cardService = cardService;
@@ -22,6 +25,7 @@ public class CoreService implements CoreServiceInterface {
     @Override
     public void validateUserToken(String token ){
         tokenService.validate(token);
+        LOGGER.info(String.format("Valid token -> %s", token));
     }
 
     @Override
@@ -30,10 +34,12 @@ public class CoreService implements CoreServiceInterface {
         User cardOwner = cardService.getCardById( cardId ).getUser();
 
         if(!tokenOwner.equals(cardOwner)) throw new CardNotMatchUserException();
+        LOGGER.info(String.format("Card %s matched user %s",cardId,cardOwner.getName()));
     }
 
     @Override
     public void checkIfAmountIsAvailable(String cardId, double amount){
          cardService.checkIfAmountIsAvailable(cardId,amount);
+         LOGGER.info(String.format("Requested amount is available on card %S",cardId));
     }
 }
