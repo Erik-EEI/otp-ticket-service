a569161d54a326baecf9457825f8e806cbee8f58
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2024-03-04 23:26:58 +0100

    Create javadoc for eventservice

--- Partner/src/main/java/com/otp/partner/service/event/EventService.java
+++ Partner/src/main/java/com/otp/partner/service/event/EventService.java
@@ -22,12 +22,22 @@ public class EventService implements EventServiceInterface {
         this.eventRepository = eventRepository;
     }
 
+    /**
+     * Retrieves all events from the database.
+     * @return A list of all events.
+     */
     @Override
     public List<Event> getAllEvents() {
         LOGGER.info("► \uD83D\uDCC2 - Getting all events from database");
         return eventRepository.findAll();
     }
 
+    /**
+     * Retrieves an event by its ID from the database.
+     * @param eventId The ID of the event to retrieve.
+     * @return The event with the specified ID.
+     * @throws EventNotFoundException If the event with the specified ID is not found.
+     */
     @Override
     public Event getEventById(Long eventId) {
         LOGGER.info(String.format("► \uD83D\uDCC2 - Getting event with ID %s from database",eventId));
